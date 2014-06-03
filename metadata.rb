ed4d5a50a93f71bcbb50150d5819635410995dd5
Hector Rivas Gandara <keymon@gmail.com>
2014-06-03 02:03:41 +0100

    Bump version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'platform-engineering@springer.com'
 license          'All rights reserved'
 description      'Installs/Configures Selenium dependencies (firefox, chrome, xvfb) and grid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'yum', '~> 3.0.6'
 
